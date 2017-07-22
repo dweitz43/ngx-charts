890596470747d7a0129f1b399fa9f3898ab90235
david weitzman <david@chorus.ai>
2017-07-22 17:26:43 +0300

    formatting

--- src/common/axes/y-axis-ticks.component.ts
+++ src/common/axes/y-axis-ticks.component.ts
@@ -83,7 +83,7 @@ import { scaleBand } from 'd3-scale';
             [attr.y]="y1"
             [attr.text-anchor]="textAnchor"
             [style.font-size]="'12px'">
-            {{trimLabel(tickFormat(refLine.value))}}
+            {{refLine.value | number:'1.0-2'}}
           </svg:text>
         </svg:g>
       </svg:g>
